7792228362ff370a48a4ab0a5db0c40fafebf155
krix38 <krix38@gmail.com>
2016-04-05 22:49:22 +0200

    added rest handlers generating clojure

--- web/controller/handler/rest/restHandler.go
+++ web/controller/handler/rest/restHandler.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-
+	
 	"github.com/krix38/ScorchedGo/properties"
 )
 
@@ -12,17 +12,27 @@ type connectionInfo struct {
 	signedIn bool
 }
 
-func ConnectionStatus(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		w.Header().Set("Content-Type", "application/json")
+var ConnectionStatus = createRestHandler(connectionStatus, []string{"GET"})
+
+func connectionStatus() interface{}{
 		/* TODO: check connection status */
-		connectionInfoStruct := connectionInfo{signedIn: false}
-		connInfo, err := json.Marshal(connectionInfoStruct)
-		if err != nil {
-			log.Fatal(properties.Messages.DebugJsonParseFail + ": connectionInfoStruct")
+		return connectionInfo{signedIn: false}
+}
+
+func createRestHandler(handler func() interface{}, acceptedMethods []string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		for _, method := range acceptedMethods {
+			if r.Method == method {
+				w.Header().Set("Content-Type", "application/json")
+				object := handler()
+				jsonObject, err := json.Marshal(object)
+				if err != nil {
+					log.Fatal(properties.Messages.DebugJsonParseFail)
+				}
+				w.Write(jsonObject)
+				return
+			}
 		}
-		w.Write(connInfo)
-	} else {
-		http.Error(w, properties.Messages.BadMethod + ": " + r.Method, http.StatusBadRequest)
+		http.Error(w, properties.Messages.BadMethod+": "+r.Method, http.StatusBadRequest)
 	}
 }
